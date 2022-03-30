9323849bd98a23986321d05c9d0a53c18cd79384
dc-andysign <58742260+dc-andysign@users.noreply.github.com>
2022-03-30 16:49:28 +0100

    Feat/f2p 66 new branding regulator (#12)
    Changed authority to regulator in start; Changed alias when anchoring authority to regulator

--- scripts/start.sh
+++ scripts/start.sh
@@ -15,7 +15,7 @@ docker-compose -f ./docker/docker-compose.yaml -p veritable-demo up --build -d
 echo -en "\n\nWaitingForIssuer"; RES=""; while [[ -z "$RES" ]]; do sleep .1; RES=$(curl -sf localhost:8021/status 2>&1); echo -n .; done
 echo -en "\n\nWaitingForHolder"; RES=""; while [[ -z "$RES" ]]; do sleep .1; RES=$(curl -sf localhost:8031/health 2>&1); echo -n .; done
 echo -en "\n\nWaitingForVerifier"; RES=""; while [[ -z "$RES" ]]; do sleep .1; RES=$(curl -sf localhost:8041/status 2>&1); echo -n .; done
-echo -en "\n\nWaitingForAuthority"; RES=""; while [[ -z "$RES" ]]; do sleep .1; RES=$(curl -sf localhost:8051/status 2>&1); echo -n .; done
+echo -en "\n\nWaitingForRegulator"; RES=""; while [[ -z "$RES" ]]; do sleep .1; RES=$(curl -sf localhost:8051/status 2>&1); echo -n .; done
 echo -e "\n\nDONE"
 
 # START REACT
@@ -23,11 +23,9 @@ echo "Starting React clients..."
 echo -en "\n\nWaitingForIssuerReact"; RES=""; while [[ -z "$RES" ]]; do sleep .1; RES=$(curl -sf localhost:3002 2>&1); echo -n .; done
 echo -en "\n\nWaitingForHolderReact"; RES=""; while [[ -z "$RES" ]]; do sleep .1; RES=$(curl -sf localhost:3001 2>&1); echo -n .; done
 echo -en "\n\nWaitingForVerifierReact"; RES=""; while [[ -z "$RES" ]]; do sleep .1; RES=$(curl -sf localhost:3003 2>&1); echo -n .; done
-echo -en "\n\nWaitingForAuthorityReact"; RES=""; while [[ -z "$RES" ]]; do sleep .1; RES=$(curl -sf localhost:3004 2>&1); echo -n .; done
+echo -en "\n\nWaitingForRegulatorReact"; RES=""; while [[ -z "$RES" ]]; do sleep .1; RES=$(curl -sf localhost:3004 2>&1); echo -n .; done
 
 # ABOUT LOGS
 echo -e "\n\nAll the backend and frontend logs are ready to be viewed."
 echo -e "\n\nDocker backend logs can be viewed with:"
-echo -e "docker logs CONTAINER_NAME (von_node1_1, ..2, ..3, ..4, von_webserver_1, consortiq, alice OR airops)"
-echo -e "\nFrontend logs can be viewed with:"
-echo -e "tail /tmp/react.log \n"
+echo -e "docker-compose -f ./docker/docker-compose.yaml -p veritable-demo logs"
